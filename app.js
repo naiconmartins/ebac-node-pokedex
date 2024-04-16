b99cd28bdbe10b496781385dc86758f1b3ff00c5
Naicon Martins <naiconmartins@hotmail.com>
2024-04-15 23:49:34 -0400

    'Adicionado views'

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 const express = require("express");
+const path = require("path");
+
 const { connect } = require("./models");
 
 const pokemonsRouter = require("./routes/pokemons");
 const app = express();
+const expressLayout = require('express-ejs-layouts');
+
+//configurando ejs
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.set(expressLayout);
 
 //declarando rotas
 app.use("/pokemons", pokemonsRouter);
 
-app.listen(3000, () => {
+const porta = 3000;
+app.listen(porta, () => {
   connect();
-
   console.log(`Servidor ouvindo na porta ${porta}`);
 });
--- routes/pokemons.js
+++ routes/pokemons.js
@@ -4,12 +4,15 @@ const { Pokemon } = require("../models");
 
 const router = express.Router();
 
-router.get("/", (res) => {
-  Pokemon.find().then((pokemons) => {
-    res.render("paginas/pokemons/index", {
-      pokemons,
+router.get("/", (req, res) => {
+  Pokemon.find()
+    .then((pokemons) => {
+      res.render("paginas/pokemons/index", { pokemons });
+    })
+    .catch((error) => {
+      console.error("Erro ao buscar pokémons:", error);
+      res.status(500).send("Erro ao processar sua solicitação");
     });
-  });
 });
 
 module.exports = router;
