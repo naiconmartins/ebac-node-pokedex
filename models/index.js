3188951112681250be899cd41b0e43e422c016d9
Naicon Martins <naiconmartins@hotmail.com>
2024-04-15 22:43:00 -0400

    Adicionado controllers

---
+++ models/index.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+const PokemonSchema = require("./pokemon");
+const Pokemon = mongoose.model("Pokemon", PokemonSchema);
+
+const connect = () => {
+  mongoose.connect("mongodb://localhost:27017/pokedex");
+};
+
+module.exports = {
+  Pokemon,
+  connect,
+};
