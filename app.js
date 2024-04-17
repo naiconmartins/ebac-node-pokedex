3010d62d5ea3f41bb20b14eae1da9278e2a86999
Naicon Martins <naiconmartins@hotmail.com>
2024-04-17 00:19:51 -0400

    CRUD

--- app.js
+++ app.js
@@ -4,16 +4,22 @@ const path = require("path");
 const { connect } = require("./models");
 
 const pokemonsRouter = require("./routes/pokemons");
+const batalhaRouter = require("./routes/batalha");
+
 const app = express();
-const expressLayout = require('express-ejs-layouts');
+const expressLayout = require("express-ejs-layouts");
 
 //configurando ejs
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
-app.set(expressLayout);
+app.use(expressLayout);
+
+//configurando arquivos estáticos
+app.use(express.static(path.join(__dirname, "public")));
 
 //declarando rotas
 app.use("/pokemons", pokemonsRouter);
+app.use("/batalha", batalhaRouter);
 
 const porta = 3000;
 app.listen(porta, () => {
---
+++ routes/batalha.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const buscaInfoPokemon = require("../services/busca-pokemon,ja");
+const router = express.Router();
+
+router.get("/", (_req, res) => {
+  const pokemonIdRandomico = Math.round(Math.random() * 904 + 1);
+  buscaInfoPokemon(pokemonIdRandomico).then((pokemon) => {
+    res.render("paginas/batalha/index", {
+      pokemon,
+    });
+  });
+});
+
+module.exports = router;
